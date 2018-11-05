d014d1c7083d9552f7e8e404b8258bd28516b12b
Cuda Chen <clh960524@gmail.com>
2018-11-05 23:46:14 +0800

    add some comments in mapper.cpp
    Add some comments in mapper.cpp for further work.

--- mapper.cpp
+++ mapper.cpp
@@ -17,4 +17,10 @@ void mapper(string inputFile, string outputFile)
 
 void mapperCheater(std::string inputFile, std::strint outputFile)
 {
+    // first use random to determine the circuit is valid
+    // 50% 50%
+
+    // then randomly choose some PIs of k-LUT
+
+    // then randomly choose some POs of k-LUT
 }
